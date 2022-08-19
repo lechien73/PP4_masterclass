ff2ac00cf9da9de312faf935de0fa915250b047f
Matt Rudge <matt.rudge@codeinstitute.net>
2022-08-19 08:10:49 +0000

    Add test for about

--- about/tests.py
+++ about/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+
+class TestView(TestCase):
+
+    def test_get_about(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
